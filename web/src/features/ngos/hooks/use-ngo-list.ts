4df09e3961c2e334b2a133c652312b2f76fa8593
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2026-04-10 22:37:55 -0300

    feat(web): add api-error type on use-ngo-list hook

--- web/src/features/ngos/hooks/use-ngo-list.ts
+++ web/src/features/ngos/hooks/use-ngo-list.ts
@@ -1,11 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
-import type { PaginationParams } from "~/shared/types/pagination";
+import type { AxiosError } from "axios";
+import type { ApiError } from "~/shared/types/api-error";
+import type { Paginated, PaginationParams } from "~/shared/types/pagination";
 import { listNGOs } from "../api";
+import type { NGO } from "../types/ngo";
 
 export const NGO_LIST_QUERY_KEY = ["ngo", "list"];
 
 export function useNgoList(pagination?: PaginationParams) {
-  return useQuery({
+  return useQuery<Paginated<NGO>, AxiosError<ApiError>>({
     queryKey: [...NGO_LIST_QUERY_KEY, pagination],
     queryFn: () => listNGOs(pagination),
     retry: false,
