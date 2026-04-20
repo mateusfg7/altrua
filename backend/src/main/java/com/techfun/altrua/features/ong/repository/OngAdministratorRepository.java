d712b177d17f773d48239f95b93c2a329b44f747
Gabriel Henrique <gabrielmkv.dev@gmail.com>
2026-04-20 18:00:50 -0300

    feat(ong/api): add method to verify if an user is a ong creator

--- backend/src/main/java/com/techfun/altrua/features/ong/repository/OngAdministratorRepository.java
+++ backend/src/main/java/com/techfun/altrua/features/ong/repository/OngAdministratorRepository.java
@@ -1,6 +1,11 @@
 package com.techfun.altrua.features.ong.repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.techfun.altrua.features.ong.domain.model.OngAdministrator;
 import com.techfun.altrua.features.ong.domain.model.OngAdministratorId;
@@ -20,4 +25,15 @@ import com.techfun.altrua.features.ong.domain.model.OngAdministratorId;
  */
 public interface OngAdministratorRepository extends JpaRepository<OngAdministrator, OngAdministratorId> {
 
+    /**
+     * Verifica se um usuário é o criador de uma ONG.
+     *
+     * @param userId o identificador único do usuário
+     * @param ongId  o identificador único da ONG
+     * @return um {@link Optional} contendo {@code true} se o usuário for o criador,
+     *         {@code false} se for administrador comum, ou vazio se não houver
+     *         vínculo
+     */
+    @Query("SELECT oa.creator FROM OngAdministrator oa WHERE oa.user.id = :userId AND oa.ong.id = :ongId")
+    public Optional<Boolean> checkIsCreator(@Param("userId") UUID userId, @Param("ongId") UUID ongId);
 }
